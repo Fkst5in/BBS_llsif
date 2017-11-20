2dce7077521cdc4253dba181b1e792732ac104b8
ad-j <adjuiminng@hfail.date>
2017-11-20 23:07:15 +0800

    ver:0.2.0 *增加发帖功能 *界面优化 *以及其他问题

--- app/src/main/java/com/example/bbs_llsif/MainActivity.java
+++ app/src/main/java/com/example/bbs_llsif/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bbs_llsif;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -12,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView text;
     String user_message_json;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //获得用户数据（主要是 User_id 和 Session）
         Intent intent = getIntent();
@@ -26,7 +32,19 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        //按钮 转向PostActivi
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent postIntent = new Intent(getApplicationContext(), PostActivity.class);
+                postIntent.putExtra("user_message", user_message_json);
+                startActivity(postIntent);
+            }
+        });
+
         doit();
+
     }
 
     public void init() {
@@ -38,6 +56,5 @@ public class MainActivity extends AppCompatActivity {
         RegistBackJson user_message = gson.fromJson(user_message_json, RegistBackJson.class);
         text.setText("User_id:" + user_message.getUser_id() + "\n" + "Session:" + user_message.getSession());
     }
-
 
 }
